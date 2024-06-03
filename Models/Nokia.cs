949d6e3305d99d64086b5b329fab5c518ea890c3
juliana <julianatelles.contato@gmail.com>
2024-06-03 15:28:18 -0300

    TODO: Herdar da classe "Smartphone" na classe Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,6 +1,11 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
 
         public override void InstalarAplicativo(string nomeApp)
         {
