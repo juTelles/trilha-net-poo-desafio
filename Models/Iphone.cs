350372b484816fd2a20530e0dbfabe4bc74b61d4
juliana <julianatelles.contato@gmail.com>
2024-06-03 15:28:41 -0300

    TODO: Herdar da classe "Smartphone" na classe Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,10 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
 
         public override void InstalarAplicativo(string nomeApp)
         {
